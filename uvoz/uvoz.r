0f2327e01ad9b82e1acc2a743f05fb66383feca1
Brina Ribič <brinaribic19@gmail.com>
2020-11-24 16:54:30 +0100

    uvozila tabelo iz Wikipedije

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -5,7 +5,7 @@ library(tidyr)
 # tabela s plačami po regijah glede na spol in starost
 
 uvoz1 <- read.csv2("podatki/SLO_place.csv", skip=2, na=c("z", "Z", "-"),
-                   encoding = "cp1250") %>%
+                   encoding="cp1250") %>%
   mutate(LETO=parse_number(LETO))
 
 spol <- function(stolpci, ime) {
@@ -22,10 +22,11 @@ skupaj <- spol(c(1, 2, 3, 6, 9, 12, 15, 18, 21),"Skupaj")
 
 place <- full_join(moski, zenske)
 place <- full_join(place, skupaj) %>% rename("STATISTICNA_REGIJA"="STATISTIČNA.REGIJA")
-place <- place %>% arrange(STATISTICNA_REGIJA) %>%
-  mutate(STATISTICNA_REGIJA=parse_factor(STATISTICNA_REGIJA)) %>%
-  mutate(SPOL=parse_factor(SPOL)) %>%
-  mutate(STAROST=parse_factor(STAROST))
+place <- place %>%
+  mutate(STATISTICNA_REGIJA=parse_factor(STATISTICNA_REGIJA, locale=locale(encoding="cp1250"))) %>%
+  mutate(SPOL=parse_factor(SPOL,locale=locale(encoding="cp1250"))) %>%
+  mutate(STAROST=parse_factor(STAROST,locale=locale(encoding="cp1250"))) %>%
+  arrange(STATISTICNA_REGIJA)
 
 
 # tabela BDP za Slovenijo
@@ -53,6 +54,33 @@ bdp_regije <- data.frame(BDP, LETO, REGIJE, bdp)
 bdp_regije <- bdp_regije %>% 
   pivot_wider(names_from = LETO, values_from = bdp) %>%
   arrange(REGIJE) %>% rename("STATISTICNA_REGIJA"="REGIJE")
+
+# tabela povprečnih plač v Evropi v EUR
+
+library(rvest)
+
+link <- "https://en.wikipedia.org/wiki/List_of_European_countries_by_average_wage"
+stran <- html_session(link) %>% read_html()
+tabela <- stran %>% 
+  html_nodes(xpath="//table[@class='wikitable sortable']") %>%
+  .[[2]] %>% 
+  html_table(fill=TRUE)
+
+place_Evropa <- tabela[c(1, 12:22)]
+place_Evropa <- place_Evropa[-c(1,2),]
+
+colnames(place_Evropa) <- c("DRZAVA", 2008:2018)
+
+place_Evropa <- place_Evropa %>% 
+  mutate(DRZAVA=gsub("\\[[^]]*\\]","", DRZAVA), DRZAVA=parse_factor(DRZAVA)) %>%
+  pivot_longer(-c(1), names_to = "LETO", values_to = "PLACE") %>%
+  mutate(PLACE=parse_number(PLACE, na=c("-", "NA"))) %>%
+  pivot_wider(names_from = LETO, values_from = PLACE)
+
+
+
+
+  
 
 
 
