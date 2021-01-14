2eda8a9b17dcf27415e8aa8eacb21bb023ae7acd
Brina Ribič <brinaribic19@gmail.com>
2021-01-14 22:40:15 +0100

    napoved za place

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,11 +1,14 @@
-# 4. faza: Analiza podatkov
+# napoved plač 
 
-podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-                                gostota.naselij=naselja/povrsina) %>%
-  left_join(povprecja, by="obcina")
-row.names(podatki) <- podatki$obcina
-podatki$obcina <- NULL
+model.plac <- lm(Placa ~ Leto, data=place)
+p <- data.frame(Leto=c(2019, 2020, 2021))
+napoved <- mutate(p, Placa=predict(model.plac,p))
 
-# Število skupin
-n <- 5
-skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
+graf.napoved1 <- ggplot(place, aes(x=Leto, y=Placa)) +
+  geom_point() + 
+  geom_smooth(method=lm, fullrange=TRUE) +
+  geom_point(data=napoved, aes(x=Leto, y=Placa), color='red') +
+  scale_x_continuous(breaks=2008:2021) +
+  xlab("Place (v EUR)") +
+  labs(title = "Napoved nadaljnih bruto plač v Sloveniji")
+
