2f743ff045773fc04a167ed31122760d3511e883
Brina Ribič <brinaribic19@gmail.com>
2021-01-29 16:06:38 +0100

    dodala graf

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,14 +1,20 @@
-# napoved plač 
+source("lib/libraries.r", encoding="UTF-8")
 
-model.plac <- lm(Placa ~ Leto, data=place)
-p <- data.frame(Leto=c(2019, 2020, 2021))
-napoved <- mutate(p, Placa=predict(model.plac,p))
+# metoda voditeljev
+placa.bdppc <- left_join(place_Evropa,bdp_pc_Evropa) %>% drop_na()
 
-graf.napoved1 <- ggplot(place, aes(x=Leto, y=Placa)) +
-  geom_point() + 
-  geom_smooth(method=lm, fullrange=TRUE) +
-  geom_point(data=napoved, aes(x=Leto, y=Placa), color='red') +
-  scale_x_continuous(breaks=2008:2021) +
-  xlab("Place (v EUR)") +
-  labs(title = "Napoved nadaljnih bruto plač v Sloveniji")
+podatki <- placa.bdppc %>%
+  select(Placa, BDP_na_prebivalca) %>%
+  as.matrix() %>%
+  scale()
 
+modelK <- kmeans(podatki, 3)
+
+skupine <- placa.bdppc %>%
+  mutate(skupina=modelK$cluster) %>%
+  ggplot(aes(x=Placa/1000,y=BDP_na_prebivalca/1000, col=Leto, shape=factor(skupina))) + 
+  geom_point(size=2) + 
+  ylab("BDP na prebivalca * 1000 (v EUR)") + 
+  xlab("Plača * 1000") + 
+  labs(title = "Primerjava BDP na prebivalca in plač v evropskih državah",shape="Skupina") +
+  theme_bw()
